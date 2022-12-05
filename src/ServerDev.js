8e89dd83c51ca3f19202b45532b5c6b86a33706b
Maor Bar Asher <94516860+MaoBA1@users.noreply.github.com>
2022-12-05 12:25:09 +0200

    123

--- src/ServerDev.js
+++ src/ServerDev.js
@@ -1,4 +1,4 @@
 export default {
-    ServerAddress: 'http://192.168.1.20:3000/api'
+    ServerAddress: 'http://192.168.1.42:3000/api'
     //ServerAddress:'https://musicboxmobile.herokuapp.com/api'
 }
